cd148fc69c287cdaeaa7bdd999c7450974a16c07
Beginner <dkstmdwn17@naver.com>
2023-10-19 22:55:04 +0900

    feat: 댓글 Entity, Repository, Service 생성

---
+++ board-back/src/main/java/com/zoo/boardback/domain/comment/application/CommentService.java
@@ -0,0 +1,14 @@
+package com.zoo.boardback.domain.comment.application;
+
+import com.zoo.boardback.domain.comment.dao.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CommentService {
+
+  private final CommentRepository commentRepository;
+}

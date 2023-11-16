1913757b9b5b653ae02eb4c6b0583d5d17febd60
Beginner <dkstmdwn17@naver.com>
2023-11-16 23:19:49 +0900

    feat: 댓글 리스트 아이템 컴포넌트 생성

---
+++ board-front/src/mocks/comment-list.mock.ts
@@ -0,0 +1,25 @@
+const commentListMock = [
+  {
+    nickname: "안녕하세요나는페이커",
+    profileImage: null,
+    writeDateTime: "3분전",
+    content:
+      "오늘 점심은 돈까스, 피자, 족발, 국밥, 버거킹, 맘스터치, 우육면, 수제비, 갈비탕 중에 하나로 먹자. 오케이",
+  },
+  {
+    nickname: "안녕하세요나는페이커",
+    profileImage: null,
+    writeDateTime: "3분전",
+    content:
+      "오늘 점심은 돈까스, 피자, 족발, 국밥, 버거킹, 맘스터치, 우육면, 수제비, 갈비탕 중에 하나로 먹자. 오케이",
+  },
+  {
+    nickname: "안녕하세요나는페이커",
+    profileImage: null,
+    writeDateTime: "3분전",
+    content:
+      "오늘 점심은 돈까스, 피자, 족발, 국밥, 버거킹, 맘스터치, 우육면, 수제비, 갈비탕 중에 하나로 먹자. 오케이",
+  },
+];
+
+export default commentListMock;

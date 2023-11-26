c7fc069334c2d6c1c5a0a1b34bf5d0379d233b9e
Beginner <dkstmdwn17@naver.com>
2023-11-26 13:27:56 +0900

    feat: 애플리케이션 라우팅 설정, 헤더 푸터 레이아웃 공통 적용 하기 위한 Container 생성

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,33 +1,54 @@
 import React, { useState } from "react";
 import "./App.css";
-import BoardItem from "components/BoardItem";
-import {
-  commentListMock,
-  favoriteListMock,
-  latestBoardListMock,
-  top3BoardListMock,
-} from "mocks";
-import Top3Item from "components/Top3Item";
-import CommentItem from "components/CommentItem";
-import { CommentListItem } from "./types/interface/comment-liset-item.interface";
-import FavoriteItem from "components/FavoriteItem";
-import InputBox from "components/InputBox";
+import { Route, Routes } from "react-router-dom";
+import Main from "views/Main";
+import Authentication from "views/Authentication";
+import BoardUpdate from "views/Board/Update";
+import Search from "views/Search";
+import User from "views/User";
+import BoardDetail from "views/Board/Detail";
+import BoardWrite from "views/Board/Write";
+import Container from "layouts/Container";
+import { MAIN_PATH } from "constant";
+import { AUTH_PATH } from "constant";
+import { SEARCH_PATH } from "constant";
+import { USER_PATH } from "constant";
+import { BOARD_PATH } from "constant";
+import { BOARD_WRITE_PATH } from "constant";
+import { BOARD_DETAIL_PATH } from "constant";
+import { BOARD_UPDATE_PATH } from "constant";
 
+//          component: Application 컴포넌트 렌더링          //
 function App() {
-  const [value, setValue] = useState<string>("");
-
+  //          render: Application 컴포넌트 렌더링          //
+  // description: 메인 화면 : '/' - Main  //
+  // description: 로그인 + 회원가입 : '/auth' - Authentication  //
+  // description: 검색 화면 : '/search/:searchWord' - Search //
+  // description: 게시물 상세보기 : '/board/detail/:boardNumber' - BoardDetail //
+  // description: 게시물 작성하기 : '/board/write' - BoardWrite //
+  // description: 게시물 수정하기 : '/board/update/:boardNumber' - BoardUpdate //
+  // description: 유저 페이지 : '/user/:userEmail' - User //
   return (
-    <>
-      <InputBox
-        label="이메일"
-        type="text"
-        placeholder="이메일 주소를 입력해주세요"
-        value={value}
-        error={true}
-        setValue={setValue}
-        message="aaaa"
-      />
-    </>
+    <Routes>
+      <Route element={<Container />}>
+        <Route path={MAIN_PATH()} element={<Main />} />
+        <Route path={AUTH_PATH()} element={<Authentication />} />
+        <Route path={SEARCH_PATH(":searchWord")} element={<Search />} />
+        <Route path={USER_PATH(":userEmail")} element={<User />} />
+        <Route path={BOARD_PATH()}>
+          <Route path={BOARD_WRITE_PATH()} element={<BoardWrite />} />
+          <Route
+            path={BOARD_DETAIL_PATH(":boardNumber")}
+            element={<BoardDetail />}
+          />
+          <Route
+            path={BOARD_UPDATE_PATH(":boardNUmber")}
+            element={<BoardUpdate />}
+          />
+        </Route>
+        <Route path="*" element={<h1>404 NOT FOUND</h1>} />
+      </Route>
+    </Routes>
   );
 }
 

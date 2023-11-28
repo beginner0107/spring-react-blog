7058a57d000235640bb4a5a5ff2b1c202ed15a6b
Beginner <dkstmdwn17@naver.com>
2023-11-28 22:51:55 +0900

    feat: Auth 페이지 공통 스타일 컴포넌트

--- board-front/src/views/Authentication/index.tsx
+++ board-front/src/views/Authentication/index.tsx
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 //          component: 인증 화면 컴포넌트         //
 export default function Authentication() {
+  //          state: 화면 상태          //
+  const [view, setView] = useState<"sign-in" | "sign-up">("sign-in");
+
+  //          component: sign in card 컴포넌트          //
+  const SignInCard = () => {
+    //        render: sign in card 컴포넌트 렌더링          //
+    return <div className="auth-card"></div>;
+  };
+  //          component: sign up card 컴포넌트          //
+  const SignUpCard = () => {
+    //        render: sign up card 컴포넌트 렌더링          //
+    return <div className="auth-card"></div>;
+  };
+
   //        render: 인증 화면 컴포넌트 렌더링          //
-  return <div>인증 화면</div>;
+  return (
+    <div id="auth-wrapper">
+      <div className="auth-conatiner">
+        <div className="auth-jumbotron-box">
+          <div className="auth-jumbotron-contents">
+            <div className="auth-logo-icon"></div>
+            <div className="auth-jumbotron-text-box">
+              <div className="auth-jumbotron-text">{"환영합니다."}</div>
+              <div className="auth-jumbotron-text">{"ZOOS BOARD 입니다."}</div>
+            </div>
+          </div>
+        </div>
+        {view === "sign-in" && <SignInCard />}
+        {view === "sign-up" && <SignUpCard />}
+      </div>
+    </div>
+  );
 }

97d7dc4cffccbd4ba46a51f4a682d182fb81aad1
Beginner <dkstmdwn17@naver.com>
2023-12-05 00:31:07 +0900

    feat: 게시물 저장 관련 Service, Controller 구현

--- board-back/src/main/java/com/zoo/boardback/domain/board/api/BoardController.java
+++ board-back/src/main/java/com/zoo/boardback/domain/board/api/BoardController.java
@@ -1,9 +1,31 @@
 package com.zoo.boardback.domain.board.api;
 
+import com.zoo.boardback.domain.auth.details.CustomUserDetails;
+import com.zoo.boardback.domain.board.application.BoardService;
+import com.zoo.boardback.domain.board.dto.request.PostCreateRequestDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/board")
 public class BoardController {
+
+  private final BoardService boardService;
+
+  @PostMapping("")
+  public ResponseEntity<Void> createBoard(
+      @RequestBody @Valid PostCreateRequestDto requestDto,
+      @AuthenticationPrincipal CustomUserDetails userDetails
+  ) {
+    boardService.create(requestDto, userDetails.getUsername());
+    return ResponseEntity.ok().build();
+  }
+
 }
--- board-back/src/main/java/com/zoo/boardback/domain/board/application/BoardService.java
+++ board-back/src/main/java/com/zoo/boardback/domain/board/application/BoardService.java
@@ -1,6 +1,17 @@
 package com.zoo.boardback.domain.board.application;
 
+import static com.zoo.boardback.global.error.ErrorCode.USER_NOT_FOUND;
+import static java.util.stream.Collectors.*;
+
 import com.zoo.boardback.domain.board.dao.BoardRepository;
+import com.zoo.boardback.domain.board.dto.request.PostCreateRequestDto;
+import com.zoo.boardback.domain.board.entity.Board;
+import com.zoo.boardback.domain.image.dao.ImageRepository;
+import com.zoo.boardback.domain.image.entity.Image;
+import com.zoo.boardback.domain.user.dao.UserRepository;
+import com.zoo.boardback.domain.user.entity.User;
+import com.zoo.boardback.global.error.BusinessException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,4 +22,24 @@ import org.springframework.transaction.annotation.Transactional;
 public class BoardService {
 
   private final BoardRepository boardRepository;
+  private final UserRepository userRepository;
+  private final ImageRepository imageRepository;
+
+  @Transactional
+  public void create(PostCreateRequestDto request, String email) {
+    User user = userRepository.findByEmail(email).orElseThrow(() ->
+        new BusinessException(email, "email", USER_NOT_FOUND));
+
+    Board board = request.toEntity(user);
+    boardRepository.save(board);
+
+    List<String> boardImageList = request.getBoardImageList();
+    List<Image> images = boardImageList.stream()
+        .map(image -> Image.builder()
+            .board(board)
+            .imageUrl(image)
+            .build())
+        .collect(toList());
+    imageRepository.saveAll(images);
+  }
 }

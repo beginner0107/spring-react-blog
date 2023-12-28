0714cb4b16e48872cdbbed13f536d3c052004410
Beginner <dkstmdwn17@naver.com>
2023-12-29 00:53:39 +0900

    refactor: 검증 로직 잘못 적용되어 있던 부분 수정

--- board-back/src/main/java/com/zoo/boardback/domain/auth/dto/request/SignUpRequestDto.java
+++ board-back/src/main/java/com/zoo/boardback/domain/auth/dto/request/SignUpRequestDto.java
@@ -3,8 +3,10 @@ package com.zoo.boardback.domain.auth.dto.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.zoo.boardback.domain.user.entity.User;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,12 +16,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @NoArgsConstructor
 public class SignUpRequestDto {
 
-  @Email
+  @Email(message = "이메일 형식을 맞춰주세요")
   private String email;
   @Pattern(regexp = "^(?=.*?[A-Za-z])(?=.*?\\d).{8,20}$", message = "비밀번호는 8~20자여야 하고 영어, 숫자가 포함되어야 합니다.")
   @JsonProperty("password")
   private String password;
-  @NotBlank(message = "닉네임은 20자 이하입니다.")
+  @NotBlank
+  @Size(max = 20, message = "닉네임은 20자 이하입니다.")
   private String nickname;
   @NotBlank @Pattern(regexp = "^[0-9]{11,13}$", message = "전화번호는 11자에서 13자 사이의 숫자만 가능합니다.")
   private String telNumber;

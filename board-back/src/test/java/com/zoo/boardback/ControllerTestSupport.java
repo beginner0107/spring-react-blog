170ee68b2cdf1994ba6b61198de45ab90747fe80
Beginner <dkstmdwn17@naver.com>
2023-12-29 00:54:15 +0900

    test: AuthController 회원가입 테스트 코드 작성

--- board-back/src/test/java/com/zoo/boardback/ControllerTestSupport.java
+++ board-back/src/test/java/com/zoo/boardback/ControllerTestSupport.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zoo.boardback.domain.auth.api.AuthController;
 import com.zoo.boardback.domain.auth.application.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+@AutoConfigureMockMvc(addFilters = false)
 @WebMvcTest(controllers = {
     AuthController.class
 })

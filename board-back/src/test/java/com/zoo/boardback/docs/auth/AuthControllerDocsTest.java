cc1e195628733ea742f8aca27224d38b934e345f
Beginner <dkstmdwn17@naver.com>
2023-12-30 00:20:48 +0900

    test: 로그인 테스트 코드 작성 및 rest docs 설정

--- board-back/src/test/java/com/zoo/boardback/docs/auth/AuthControllerDocsTest.java
+++ board-back/src/test/java/com/zoo/boardback/docs/auth/AuthControllerDocsTest.java
@@ -1,19 +1,25 @@
 package com.zoo.boardback.docs.auth;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.zoo.boardback.docs.RestDocsSupport;
 import com.zoo.boardback.domain.auth.api.AuthController;
 import com.zoo.boardback.domain.auth.application.AuthService;
+import com.zoo.boardback.domain.auth.dto.request.SignInRequestDto;
 import com.zoo.boardback.domain.auth.dto.request.SignUpRequestDto;
+import com.zoo.boardback.domain.auth.dto.response.SignInResponseDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
@@ -61,6 +67,42 @@ public class AuthControllerDocsTest extends RestDocsSupport {
         ));
   }
 
+  @DisplayName("회원의 이메일과 비밀번호를 입력하면 로그인을 할 수 있다.")
+  @Test
+  void signIn() throws Exception {
+    SignInRequestDto signInRequest = createSignInRequest("test123@naver.com", "test12324dpass");
+    String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ikpv";
+    given(authService.signIn(any(SignInRequestDto.class))).willReturn(
+        SignInResponseDto.builder()
+            .token(token)
+            .expirationTime(3600)
+            .build());
+
+    mockMvc.perform(
+            post("/api/v1/auth/sign-in")
+                .content(objectMapper.writeValueAsString(signInRequest))
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(csrf())
+        )
+        .andExpect(status().isOk())
+        .andDo(document("auth-sign-in",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint()),
+            requestFields(
+                fieldWithPath("email").type(JsonFieldType.STRING)
+                    .description("이메일"),
+                fieldWithPath("password").type(JsonFieldType.STRING)
+                    .description("비밀번호")
+            ),
+            responseFields(
+                fieldWithPath("token").type(JsonFieldType.STRING)
+                    .description("엑세스 토큰"),
+                fieldWithPath("expirationTime").type(JsonFieldType.NUMBER)
+                    .description("토큰 만료시간")
+            )
+        ));
+  }
+
   private SignUpRequestDto createSignUpRequest(String userEmail, String password, String nickname, String telNumber) {
     return SignUpRequestDto.builder()
         .email(userEmail)
@@ -69,6 +111,13 @@ public class AuthControllerDocsTest extends RestDocsSupport {
         .telNumber(telNumber)
         .address("경기도 용인시 기흥구")
         .addressDetail(null)
+        .build();
+  }
+
+  private SignInRequestDto createSignInRequest(String email, String password) {
+    return SignInRequestDto.builder()
+        .email(email)
+        .password(password)
         .build();
   }
 }

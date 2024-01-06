1ec77f8948f8fe4b7aa9389eba748d31aeb9b756
Beginner <dkstmdwn17@naver.com>
2024-01-06 16:47:27 +0900

    refactor: 메서드 부분을 inline으로 설정

--- board-back/src/main/java/com/zoo/boardback/domain/file/api/FileController.java
+++ board-back/src/main/java/com/zoo/boardback/domain/file/api/FileController.java
@@ -1,5 +1,6 @@
 package com.zoo.boardback.domain.file.api;
 
+import com.zoo.boardback.domain.ApiResponse;
 import com.zoo.boardback.global.util.file.FileUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
@@ -20,16 +21,14 @@ public class FileController {
   private final FileUtil fileUtil;
 
   @PostMapping("/upload")
-  public String upload(@RequestParam("file")MultipartFile file) {
-    String url = fileUtil.upload(file);
-    return url;
+  public ApiResponse<String> upload(@RequestParam("file")MultipartFile file) {
+    return ApiResponse.ok(fileUtil.upload(file));
   }
 
   @GetMapping(value="{fileName}", produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
   public Resource getImage(
       @PathVariable("fileName") String fileName
   ) {
-    Resource resource = fileUtil.getImage(fileName);
-    return resource;
+    return fileUtil.getImage(fileName);
   }
 }

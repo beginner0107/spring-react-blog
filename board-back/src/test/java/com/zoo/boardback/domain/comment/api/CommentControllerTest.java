121707dab66fee82999789f73e1321f7514de260
Beginner <dkstmdwn17@naver.com>
2024-01-11 22:51:07 +0900

    test: Controller 테스트 코드 작성 - 실패 케이스 작성

--- board-back/src/test/java/com/zoo/boardback/domain/comment/api/CommentControllerTest.java
+++ board-back/src/test/java/com/zoo/boardback/domain/comment/api/CommentControllerTest.java
@@ -15,10 +15,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.zoo.boardback.ControllerTestSupport;
 import com.zoo.boardback.WithAuthUser;
+import com.zoo.boardback.domain.comment.dto.query.CommentQueryDto;
 import com.zoo.boardback.domain.comment.dto.request.CommentCreateRequestDto;
 import com.zoo.boardback.domain.comment.dto.request.CommentUpdateRequestDto;
 import com.zoo.boardback.domain.comment.dto.response.CommentListResponseDto;
-import com.zoo.boardback.domain.comment.dto.response.CommentResponse;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -29,7 +30,7 @@ class CommentControllerTest extends ControllerTestSupport {
   @DisplayName("댓글에 필요한 정보를 입력 후 등록을 하면 댓글이 저장된다.")
   @WithAuthUser(email = "test123@naver.com", role = "ROLE_USER")
   @Test
-  void createComment() throws Exception {
+  void createCommentEmptyBoardNumber() throws Exception {
     // given
     String content = "댓글내용입니다.";
     int boardNumber = 1;
@@ -47,6 +48,28 @@ class CommentControllerTest extends ControllerTestSupport {
     then(commentService).should(times(1)).create(any(), any());
   }
 
+  @DisplayName("댓글 내용을 입력하지 않으면 댓글을 작성할 수 없습니다.")
+  @WithAuthUser(email = "test123@naver.com", role = "ROLE_USER")
+  @Test
+  void createComment() throws Exception {
+    // given
+    String content = "";
+    int boardNumber = 1;
+    CommentCreateRequestDto request = createComment(boardNumber, content);
+
+    // when & then
+    mockMvc.perform(post("/api/v1/comments")
+            .content(objectMapper.writeValueAsString(request))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(jsonPath("$.code").value(400))
+        .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+        .andExpect(jsonPath("$.message").value("댓글 내용을 입력해주세요"))
+        .andExpect(jsonPath("$.field").value("content"))
+        .andExpect(jsonPath("$.data").isEmpty())
+        .andExpect(status().isBadRequest());
+  }
+
   @DisplayName("게시글의 댓글의 목록은 누구나 조회할 수 있다.")
   @Test
   void getComments() throws Exception {
@@ -54,17 +77,21 @@ class CommentControllerTest extends ControllerTestSupport {
     int boardNumber = 1;
     CommentListResponseDto responseDto = CommentListResponseDto.from(
         List.of(
-            CommentResponse.builder()
+            CommentQueryDto.builder()
                 .commentNumber(2)
                 .content("댓글 작성2")
                 .nickname("닉네임2")
                 .profileImage("http://localhost:8080/image2.png")
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
                 .build(),
-            CommentResponse.builder()
+            CommentQueryDto.builder()
                 .commentNumber(1)
                 .content("댓글 작성1")
                 .nickname("닉네임1")
                 .profileImage("http://localhost:8080/image1.png")
+                .createdAt(LocalDateTime.now().plusHours(1))
+                .updatedAt(LocalDateTime.now().plusHours(1))
                 .build()
         )
     );
@@ -107,8 +134,34 @@ class CommentControllerTest extends ControllerTestSupport {
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.code").value(200))
         .andExpect(jsonPath("$.status").value("OK"))
-        .andExpect(jsonPath("$.message").value("OK"));
+        .andExpect(jsonPath("$.message").value("OK"))
+        .andExpect(jsonPath("$.field").isEmpty())
+        .andExpect(jsonPath("$.data").isEmpty());
     then(commentService).should(times(1)).editComment(anyInt(), any());
+  }
+
+  @DisplayName("댓글의 내용을 입력하지 않으면 댓글이 수정되지 않는다.")
+  @WithAuthUser(email = "test123@naver.com", role = "ROLE_USER")
+  @Test
+  void editCommentEmptyContent() throws Exception {
+    // given
+    String content = "";
+    int commentNumber = 1;
+    CommentUpdateRequestDto requestDto = CommentUpdateRequestDto.builder()
+        .boardNumber(1)
+        .content(content).build();
+
+    // when & then
+    mockMvc.perform(put("/api/v1/comments/{commentNumber}", commentNumber)
+            .content(objectMapper.writeValueAsString(requestDto))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("$.code").value(400))
+        .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+        .andExpect(jsonPath("$.message").value("댓글 내용을 입력해주세요"))
+        .andExpect(jsonPath("$.field").value("content"))
+        .andExpect(jsonPath("$.data").isEmpty());
   }
 
   @DisplayName("회원은 댓글을 삭제할 수 있다.")
@@ -130,7 +183,7 @@ class CommentControllerTest extends ControllerTestSupport {
     then(commentService).should(times(1)).deleteComment(anyInt(), anyInt());
   }
 
-  private CommentCreateRequestDto createComment(int boardNumber, String content) {
+  private CommentCreateRequestDto createComment(Integer boardNumber, String content) {
     return CommentCreateRequestDto.builder()
         .boardNumber(boardNumber)
         .content(content)

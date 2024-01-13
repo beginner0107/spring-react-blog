15d64eb8926a2e634f4e887a077940355d8c3462
Beginner <dkstmdwn17@naver.com>
2024-01-14 00:34:14 +0900

    refactor: equals and hashcode 추가

--- board-back/src/main/java/com/zoo/boardback/domain/auth/entity/Authority.java
+++ board-back/src/main/java/com/zoo/boardback/domain/auth/entity/Authority.java
@@ -1,19 +1,26 @@
 package com.zoo.boardback.domain.auth.entity;
 
+import static lombok.AccessLevel.PROTECTED;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.zoo.boardback.domain.user.entity.User;
 import com.zoo.boardback.global.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+@NoArgsConstructor(access = PROTECTED)
+@EqualsAndHashCode(of = {"id"}, callSuper = false)
 public class Authority extends BaseEntity {
 
   @Id
@@ -30,5 +37,11 @@ public class Authority extends BaseEntity {
 
   public void setUser(User user) {
     this.users = user;
+  }
+
+  @Builder
+  public Authority(String name, User users) {
+    this.name = name;
+    this.users = users;
   }
 }
--- board-back/src/main/java/com/zoo/boardback/domain/board/entity/Board.java
+++ board-back/src/main/java/com/zoo/boardback/domain/board/entity/Board.java
@@ -14,6 +14,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,6 +22,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = PROTECTED)
 @Entity
 @Table(name = "Board")
+@EqualsAndHashCode(of = {"boardNumber"}, callSuper = false)
 public class Board extends BaseEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)

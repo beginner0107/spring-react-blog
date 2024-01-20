a63b0adf35bcef08c94706de4e5c02e942b7b7e5
Beginner <dkstmdwn17@naver.com>
2024-01-20 21:19:09 +0900

    refactor: DTO 빌더 패턴으로 변경

--- board-back/src/main/java/com/zoo/boardback/domain/board/dto/response/PostSearchResponseDto.java
+++ board-back/src/main/java/com/zoo/boardback/domain/board/dto/response/PostSearchResponseDto.java
@@ -1,12 +1,11 @@
 package com.zoo.boardback.domain.board.dto.response;
 
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class PostSearchResponseDto {
   private String profileImage;
@@ -18,4 +17,19 @@ public class PostSearchResponseDto {
   private Integer favoriteCount;
   private Integer commentCount;
   private String boardTitleImage;
+
+  @Builder
+  public PostSearchResponseDto(String profileImage, String nickname, LocalDateTime createdAt,
+      String title, String content, Integer viewCount, Integer favoriteCount, Integer commentCount,
+      String boardTitleImage) {
+    this.profileImage = profileImage;
+    this.nickname = nickname;
+    this.createdAt = createdAt;
+    this.title = title;
+    this.content = content;
+    this.viewCount = viewCount;
+    this.favoriteCount = favoriteCount;
+    this.commentCount = commentCount;
+    this.boardTitleImage = boardTitleImage;
+  }
 }

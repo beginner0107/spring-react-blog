0c6a8052c9bc23998ebcf7d30309c7f1c72b671d
Beginner <dkstmdwn17@naver.com>
2024-03-03 15:14:40 +0900

    refactor: JPA 쿼리 메서드 기능 활용

--- board-back/src/main/java/com/zoo/boardback/domain/comment/dao/CommentRepository.java
+++ board-back/src/main/java/com/zoo/boardback/domain/comment/dao/CommentRepository.java
@@ -1,7 +1,7 @@
 package com.zoo.boardback.domain.comment.dao;
 
-import com.zoo.boardback.domain.post.entity.Post;
 import com.zoo.boardback.domain.comment.entity.Comment;
+import com.zoo.boardback.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,7 +9,5 @@ import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
 
-    @Modifying
-    @Query("DELETE FROM Comment i WHERE i.post = :post")
-    void deleteByPost(@Param("post") Post post);
+    void deleteByPostId(Long postId);
 }

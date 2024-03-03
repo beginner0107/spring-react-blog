fca519b71d2507e2557b81167d828b2442d80cd5
Beginner <dkstmdwn17@naver.com>
2024-03-03 18:47:06 +0900

    refactor: FavoriteService 내부 메서드 분리 및 정리

--- board-back/src/main/java/com/zoo/boardback/domain/favorite/application/FavoriteService.java
+++ board-back/src/main/java/com/zoo/boardback/domain/favorite/application/FavoriteService.java
@@ -1,5 +1,6 @@
 package com.zoo.boardback.domain.favorite.application;
 
+import static com.zoo.boardback.global.error.ErrorCode.*;
 import static com.zoo.boardback.global.error.ErrorCode.POST_NOT_FOUND;
 import static com.zoo.boardback.global.error.ErrorCode.USER_NOT_FOUND;
 
@@ -13,7 +14,7 @@ import com.zoo.boardback.domain.post.entity.Post;
 import com.zoo.boardback.domain.user.dao.UserRepository;
 import com.zoo.boardback.domain.user.entity.User;
 import com.zoo.boardback.global.error.BusinessException;
-import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -30,44 +31,57 @@ public class FavoriteService {
 
     @Transactional
     public void like(Long postId, String email) {
-        Post post = postRepository.findById(postId).orElseThrow(() ->
-            new BusinessException(postId, "postId", POST_NOT_FOUND));
+        Post post = findPostByPostId(postId);
+        User user = findUserByEmail(email);
 
-        User user = userRepository.findByEmail(email).orElseThrow(() ->
-            new BusinessException(email, "email", USER_NOT_FOUND));
         FavoritePk favoritePk = new FavoritePk(post, user);
-        Favorite favorite = favoriteRepository.findByFavoritePk(favoritePk);
-        if (favorite == null) {
-            favorite = Favorite.builder()
-                .favoritePk(favoritePk)
-                .build();
-            favoriteRepository.save(favorite);
-            post.increaseFavoriteCount();
+        validateFavoriteAlreadyExists(favoritePk, post);
+        doLike(favoritePk, post);
+    }
+
+    private void validateFavoriteAlreadyExists(FavoritePk favoritePk, Post post) {
+        Optional<Favorite> favoriteOp = favoriteRepository.findByFavoritePk(favoritePk);
+        if (favoriteOp.isPresent()) {
+            throw new BusinessException(post.getId(), "postId", FAVORITE_ALREADY_EXISTS);
         }
+    }
+
+    private void doLike(FavoritePk favoritePk, Post post) {
+        Favorite favorite = Favorite.create(favoritePk);
+        favoriteRepository.save(favorite);
+        post.increaseFavoriteCount();
     }
 
     @Transactional
     public void cancelLike(Long postId, String email) {
-        Post post = postRepository.findById(postId).orElseThrow(() ->
-            new BusinessException(postId, "postId", POST_NOT_FOUND));
+        Post post = findPostByPostId(postId);
 
-        User user = userRepository.findByEmail(email).orElseThrow(() ->
-            new BusinessException(email, "email", USER_NOT_FOUND));
+        User user = findUserByEmail(email);
         FavoritePk favoritePk = new FavoritePk(post, user);
-        Favorite favorite = favoriteRepository.findByFavoritePk(favoritePk);
-        if (favorite != null) {
-            favoriteRepository.delete(favorite);
-            post.decreaseFavoriteCount();
-        }
+        doCancelLike(favoritePk, post);
+    }
+
+    private void doCancelLike(FavoritePk favoritePk, Post post) {
+        favoriteRepository.delete(favoriteRepository.findByFavoritePk(favoritePk)
+            .orElseThrow(() -> new BusinessException(post.getId(), "postId", FAVORITE_CANCEL)));
+        post.decreaseFavoriteCount();
     }
 
     public FavoriteListResponseDto getFavorites(Long postId) {
-        Post post = postRepository.findById(postId).orElseThrow(() ->
-            new BusinessException(postId, "postId", POST_NOT_FOUND));
-        List<Favorite> favorites = favoriteRepository.findRecommendersByPost(post);
-        List<FavoriteQueryDto> favoritesQueryDtos = favorites.stream()
+        Post post = findPostByPostId(postId);
+        return FavoriteListResponseDto.from(
+            favoriteRepository.findFavoritesByPost(post).stream()
             .map(FavoriteQueryDto::from)
-            .collect(Collectors.toList());
-        return FavoriteListResponseDto.from(favoritesQueryDtos);
+            .collect(Collectors.toList()));
+    }
+
+    private Post findPostByPostId(Long postId) {
+        return postRepository.findById(postId).orElseThrow(() ->
+            new BusinessException(postId, "postId", POST_NOT_FOUND));
+    }
+
+    private User findUserByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() ->
+            new BusinessException(email, "email", USER_NOT_FOUND));
     }
 }

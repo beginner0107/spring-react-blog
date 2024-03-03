198d851fa9bdd1de25cc547c32076756676c6a01
Beginner <dkstmdwn17@naver.com>
2024-03-03 15:44:19 +0900

    refactor: 문자열 하드코딩 -> 상수로 분리

--- board-back/src/main/java/com/zoo/boardback/domain/post/application/PostCacheService.java
+++ board-back/src/main/java/com/zoo/boardback/domain/post/application/PostCacheService.java
@@ -16,6 +16,12 @@ public class PostCacheService {
 
     private final PostRepository postRepository;
     private final RedisUtil redisUtil;
+    private static final String CACHE_REFRESH_CRON_EXPRESSION = "0 0/3 * * * ?";
+    private static final String POST_VIEW_COUNT_CACHE_PREFIX = "postViewCount";
+    private static final String KEY_SEPARATOR = "::";
+    private static final String MATCH_ANY = "*";
+    public static final long CACHE_EXPIRATION_TIME_MINUTES = 3L;
+
 
     public void addViewCntToRedis(Long postId) {
         String viewCntKey = createViewCntCacheKey(postId);
@@ -24,10 +30,10 @@ public class PostCacheService {
             return;
         }
 
-        redisUtil.setData(
+        redisUtil.setDataExpire(
             viewCntKey,
             String.valueOf(postRepository.findViewCount(postId) + 1),
-            Duration.ofMinutes(3)
+            CACHE_EXPIRATION_TIME_MINUTES
         );
     }
 
@@ -35,9 +41,9 @@ public class PostCacheService {
      * 3분마다 캐시 데이터를 RDB 반영 후 삭제한다
      */
     @Transactional
-    @Scheduled(cron = "0 0/3 * * * ?")
+    @Scheduled(cron = CACHE_REFRESH_CRON_EXPRESSION)
     public void applyViewCountToRDB() {
-        Set<String> viewCntKeys = redisUtil.keys("postViewCount*");
+        Set<String> viewCntKeys = redisUtil.keys(POST_VIEW_COUNT_CACHE_PREFIX + MATCH_ANY);
         if (Objects.requireNonNull(viewCntKeys).isEmpty()) {
             return;
         }
@@ -53,14 +59,14 @@ public class PostCacheService {
     }
 
     private Long extractPostIdFromKey(String key) {
-        return Long.parseLong(key.split("::")[1]);
+        return Long.parseLong(key.split(KEY_SEPARATOR)[1]);
     }
 
     private String createViewCntCacheKey(Long id) {
-        return createCacheKey("postViewCount", id);
+        return createCacheKey(POST_VIEW_COUNT_CACHE_PREFIX, id);
     }
 
     private String createCacheKey(String cacheType, Long id) {
-        return cacheType + "::" + id;
+        return cacheType + KEY_SEPARATOR + id;
     }
 }
